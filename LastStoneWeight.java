190c85189fbb3beae611c1a0ad105f83a01f848d
AathiEswar <aathi28082003@gmail.com>
2024-06-06 10:51:18 +0530

    6-6-2024

--- LastStoneWeight.java
+++ LastStoneWeight.java
@@ -1,10 +1,11 @@
 package NeetCode;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class LastStoneWeight {
     public int lastStoneWeight(int[] stones){
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         for(int stone : stones){
             queue.add(stone);
         }
