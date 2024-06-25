55a1c20076f2f9d8109ce0e4ffcac1e26cb48683
AathiEswar <aathi28082003@gmail.com>
2024-06-25 10:32:30 +0530

    25-6-2024

---
+++ FindPivotIndexArray.java
@@ -0,0 +1,24 @@
+package NeetCode;
+
+public class FindPivotIndexArray {
+    public int findPivot(int[] nums){
+        int total = 0;
+
+        for(int num : nums){
+            total += num;
+        }
+
+        int right = 0 ;
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            right = total - nums[i] - left;
+
+            if(left == right)return i;
+
+            left += nums[i];
+        }
+
+        return -1;
+    }
+}
