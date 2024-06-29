a4effdde94a0b2bd7c081687234461a9c87612d4
AathiEswar <aathi28082003@gmail.com>
2024-06-29 07:58:39 +0530

    29-6-2024

---
+++ GuessNum.java
@@ -0,0 +1,26 @@
+package NeetCode;
+
+public class GuessNum {
+    public int guessNumber(int n) {
+        int start = 1 ;
+        int end = n;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            int guessNum = guess(mid);
+
+            if(guessNum == 0){
+                return mid;
+            }
+            if(guessNum == 1){
+                start =mid+1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return n;
+    }
+}
